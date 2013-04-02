342c47263e52083a00a3dca9bf4c03c0c7392920
luggage <mud@second-contract.com>
2013-04-01 21:29:04 -0400

    updated to network-enabled dgd/kernel lib

--- mud/include/config.h
+++ mud/include/config.h
@@ -3,6 +3,13 @@
 # define CLONABLE_SUBDIR	"/obj/"
 # define LIGHTWEIGHT_SUBDIR	"/data/"
 
+/*
+ * Support both the old pre 1.4 network package, and 1.4's network extensions
+ */
+# if defined( __NETWORK_PACKAGE__ ) || defined( __NETWORK_EXTENSIONS__ )
+#  define SYS_NETWORKING        /* Network package is enabled */
+# endif
+
 # define SYS_PERSISTENT			/* off by default */
 
 # define SYSTEM_USER    "/usr/System/obj/user"
